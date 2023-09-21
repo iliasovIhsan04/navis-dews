924a554df226203e6150d8300e818133540b1552
liasovIhsan04 <liasovihsan1@gmail.com>
2023-09-21 21:11:29 +0600

    ihsan

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route />
       </Routes>
-      <h1>Ihsan</h1>
+      <h1 className="title_ihsan">NAVIS-DEWS</h1>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,36 @@
 import React from "react";
 import "./Header.css";
+import { NavLink } from "react-router-dom";
 const Header = () => {
-  return <div className="header">
-    
-  </div>;
+  return (
+    <div className="header">
+      <div className="container">
+        <div className="block">
+          <NavLink to="/" className="page">
+            Главная
+          </NavLink>
+          <NavLink to="/Ihsan" className="page">
+            Ihsan
+          </NavLink>
+          <NavLink to="Beka" className="page">
+            Beka
+          </NavLink>
+          <NavLink to="ilim" className="page">
+            Ilim
+          </NavLink>
+          <NavLink to="kursant" className="page">
+            Kursant
+          </NavLink>
+          <NavLink to="chyngyz" className="page">
+            Chyngyz
+          </NavLink>
+          <NavLink to="baary" className="page">
+            Баары
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
