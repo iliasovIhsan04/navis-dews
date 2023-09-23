7ab5b37c9679a6afa8e4acf6a9564e826fc6c1f8
ilimbek003 <kaiyrbekuulualumbek@gmail.com>
2023-09-23 12:59:59 +0600

    fds

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from "./App";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { BrowserRouter } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
