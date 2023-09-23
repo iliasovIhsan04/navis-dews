f63c18c70c1a596040f37ba18312f6e6a782b530
ilimbek003 <kaiyrbekuulualumbek@gmail.com>
2023-09-23 13:47:09 +0600

    fd

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./Main.css";
 import Home from "../Home/Home";
 import Section from "../Section/Section";
-
+import NevidevsProjects from "../ NevidevsProjects/NevidevsProjects";
 const Main = () => {
   return (
     <div className="main">
         <Home />
         <Section/>
+        <NevidevsProjects/>
     </div>
   );
 };
