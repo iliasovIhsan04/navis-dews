aab504df91889d7b4406feb53b43e3d3d3758704
liasovIhsan04 <liasovihsan1@gmail.com>
2023-09-23 12:59:12 +0600

    ihsan

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import "./Section.css";
+import image1 from "../../img/image1.svg";
+import image2 from "../../img/image2.svg";
+import phone from "../../img/iphone.jpg";
+import search from "../../img/search.svg";
+import game from "../../img/image 405.svg";
+import planshet from "../../img/planshet.svg";
+import ui from "../../img/ui.svg";
 
 const Section = () => {
   return (
@@ -17,6 +24,7 @@ const Section = () => {
               Разработка удобной и заточенной под клиента системы управления
               сайтом
             </p>
+            <img src={image1} alt="" />
           </div>
           <div className="grid_box">
             <h1>Адаптивный сайт</h1>
@@ -24,6 +32,7 @@ const Section = () => {
               Создание адаптивного сайта под все существующие платформы и
               браузеры
             </p>
+            <img src="" alt="" />
           </div>
           <div className="grid_box">
             <h1>Анализ рынка и конкурентов</h1>
@@ -31,10 +40,37 @@ const Section = () => {
               Разработка концепции сайта, на основе анализа рынка, конкурентов и
               тенденций
             </p>
+            <img src={search} alt="" />
           </div>
-          <div className="grid_box"></div>
-          <div className="grid_box"></div>
-          <div className="grid_box"></div>
+          <div className="grid_box">
+            <h1>SEO Оптимизация</h1>
+            <p>
+              Внутренняя SEO оптимизация сайта под поисковики Google и Яндекс
+            </p>
+            <img src={game} alt="" />
+          </div>
+          <div className="grid_box">
+            <h1>
+              Контент
+              <h3>Сайта</h3>
+            </h1>
+            <p>
+              При необходимости написание вкусного, продающего контента для
+              сайта
+            </p>
+            <img src={planshet} alt="" />
+          </div>
+          <div className="grid_box">
+            <h1>
+              UX UI
+              <h3>Дизайн</h3>
+            </h1>
+            <p>
+              Разработка концепции сайта, на основе анализа рынка, конкурентов и
+              тенденций
+            </p>
+            <img src={ui} alt="" />
+          </div>
         </div>
       </div>
     </div>
