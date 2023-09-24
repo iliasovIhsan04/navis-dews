da7ba3047af29b2e045ed9870a462401caf3a745
liasovIhsan04 <liasovihsan1@gmail.com>
2023-09-25 01:17:00 +0600

    ihsan

--- src/components/ NevidevsProjects/NevidevsProjects.jsx
+++ src/components/ NevidevsProjects/NevidevsProjects.jsx
@@ -44,32 +44,32 @@ const NevidevsProjects = () => {
     autoplay: true,
     autoplaySpeed: 2000,
     cssEase: "linear",
-    responsive: [
-      {
-        breakpoint: 700,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 500,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
+    // responsive: [
+    //   {
+    //     breakpoint: 700,
+    //     settings: {
+    //       slidesToShow: 2,
+    //       slidesToScroll: 1,
+    //       infinite: true,
+    //       dots: true,
+    //     },
+    //   },
+    //   {
+    //     breakpoint: 600,
+    //     settings: {
+    //       slidesToShow: 2,
+    //       slidesToScroll: 2,
+    //       initialSlide: 2,
+    //     },
+    //   },
+    //   {
+    //     breakpoint: 500,
+    //     settings: {
+    //       slidesToShow: 1,
+    //       slidesToScroll: 1,
+    //     },
+    //   },
+    // ],
   };
   return (
     <div className="nevidevs_projects">
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,13 +11,12 @@ const Header = () => {
       <div className="container">
         <div className="block_all">
           <h1> nevisdevs.</h1>
+          <div className="burger_block">
+            <div className="burger_box1"></div>
+            <div className="burger_box2"></div>
+          </div>
           <div className="block">
-            <a to="/Ihsan" className="page">
-              <div className="burger_block">
-                <div className="burger_box1"></div>
-                <div className="burger_box2"></div>
-              </div>
-            </a>
+            <a to="/Ihsan" className="page"></a>
             <a to="directions" className="page">
               направления
             </a>
@@ -44,22 +43,28 @@ const Header = () => {
                 x
               </span>
               <h1>Заполните данные, чтобы получить бесплатную консультацию</h1>
-              <div className="input_block">
-                <input className="input_box" placeholder="Введите Ваше имя" />
-                <input className="input_box" placeholder="Введите Ваш номер" />
-                <textarea
-                  className="input_box"
-                  placeholder="Что вас интересует?"
-                  name=""
-                  id=""
-                  cols="30"
-                  rows="10"
-                ></textarea>
-              </div>
-              <button className="modal_btn">
-                Оставить заявку{" "}
-                <GoArrowUpRight size={25} style={{ marginLeft: "20px" }} />
-              </button>
+              <form action="">
+                <div className="input_block">
+                  <input className="input_box" placeholder="Введите Ваше имя" />
+                  <input
+                    className="input_box"
+                    placeholder="Введите Ваш номер"
+                  />
+                  <textarea
+                    className="input_box"
+                    placeholder="Что вас интересует?"
+                    name=""
+                    id=""
+                    cols="30"
+                    rows="10"
+                  ></textarea>
+                </div>
+                <button className="modal_btn">
+                  Оставить заявку{" "}
+                  <GoArrowUpRight size={25} style={{ marginLeft: "20px" }} />
+                </button>
+              </form>
+
               <p>
                 Нажимая на кнопку, вы даете согласие на обработку своих
                 персональных данных и соглашаетесь
